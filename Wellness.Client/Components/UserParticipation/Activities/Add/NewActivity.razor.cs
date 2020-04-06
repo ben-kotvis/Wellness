d111536fd66a07700d59fd6989d0efc481d6a165
Ben Kotvis <Ben.Kotvis@insight.com>
2020-04-06 09:01:21 -0500

    More MatBlazor stuff

---
+++ Wellness.Client/Components/UserParticipation/Activities/Add/NewActivity.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wellness.Client.Components.UserParticipation.Activities.Add
+{
+    public class NewActivityComponent : ComponentBase
+    {
+        public string SelectedId { set; get; } = Guid.Empty.ToString();
+
+        public string SelectedActivity { get; set; }
+        public string NumberOfMinutes { get; set; }
+        public DateTime SelectedDate { get; set; } = DateTime.MinValue;
+    }
+}
---
+++ Wellness.Client/Components/UserParticipation/Activities/List/UserActivities.razor.cs
@@ -0,0 +1,73 @@
+﻿using MatBlazor;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wellness.Model;
+
+namespace Wellness.Client.Components.UserParticipation.Activities.List
+{
+    public class UserActivitiesComponent : ComponentBase
+    {
+
+        [Inject] AppState State { get; set; }
+
+        [Parameter]
+        public List<ActivityParticipation> ActivityParticipations { get; set; }
+
+
+
+        private string SelectedValue => SelectedId == default ? default : State.Activities.First(i => i.Id == SelectedId).Name;
+
+        private Guid SelectedId { set; get; } = default;
+
+        public BaseMatIconButton activityItemButton;
+        public BaseMatMenu activityItemMenu;
+
+        public void OnClick(MouseEventArgs e)
+        {
+            this.activityItemMenu.OpenAsync(activityItemButton.Ref);
+        }
+
+        private void UpdateSelectedIndex(Guid id)
+        {
+            SelectedId = id;
+
+            Open = false;
+
+            StateHasChanged();
+        }
+
+        private void HandleClose(EventArgs args)
+        {
+            Open = false;
+
+            StateHasChanged();
+        }
+
+        private void HandleClickListItem()
+        {
+            Open = true;
+
+            StateHasChanged();
+        }
+
+        private void UpdateSelected(string id)
+        {
+
+        }
+
+        bool Open = false;
+        private object Value { set; get; } = 0;
+
+        private void HandleChange(object value)
+        {
+            Value = value;
+
+            StateHasChanged();
+        }
+
+    }
+}
