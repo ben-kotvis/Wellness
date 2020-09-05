f086056c300eef82eb5f49a7c42679ce58504172
kotvisbj <kotvisbj@gmail.com>
2020-09-04 19:56:01 -0500

    B2C

---
+++ Wellness.Authorization.Api/ReaderService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using Wellness.Model;
+
+namespace Wellness.Authorization.Api
+{
+    public class ReaderService<T> : IReaderService<T>
+        where T : IIdentifiable
+    {
+        private readonly IPersistanceReaderService<T> _persistanceReaderService;
+        private readonly ClaimsPrincipal _claimsPrincipal;
+        private readonly Guid _companyId;
+        public ReaderService(
+            IPersistanceReaderService<T> persistanceReaderService, ClaimsPrincipal claimsPrincipal)
+        {
+            _persistanceReaderService = persistanceReaderService;
+            _claimsPrincipal = claimsPrincipal;
+            _companyId = Guid.Parse(claimsPrincipal.FindFirstValue("companyId"));
+        }
+
+        public Task<PersistenceWrapper<T>> Get(Guid id, CancellationToken cancellationToken)
+        {
+            return _persistanceReaderService.Get(id, _companyId, cancellationToken);
+        }
+
+        public Task<IEnumerable<PersistenceWrapper<T>>> GetAll(CancellationToken cancellationToken)
+        {
+            return _persistanceReaderService.GetAll(_companyId, cancellationToken);
+        }
+    }
+}
